0a88b51c2c5e90656f72d07692ad8fa497c7f515
Luis Enrique Solis Navarro <luisenrique.solisnavarro@gmail.com>
2020-07-01 18:47:44 -0600

    testing development branch

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import MainLayout from '../src/components/MainLayout'
 export default function Home() {
   return (
     <MainLayout title="Inicio">
-      <h1>Hello next js</h1>
+      <h1>Hello next development branchjs</h1>
     </MainLayout>  
   )
 }
