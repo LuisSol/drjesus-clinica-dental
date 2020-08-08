0fb2b875a6db7696a60462871aabe8506eec796c
Luis Enrique Solis Navarro <luisenrique.solisnavarro@gmail.com>
2020-08-08 16:41:11 -0500

    fixed bug on contact Form

--- pages/api/contact_form.js
+++ pages/api/contact_form.js
@@ -1,47 +1,50 @@
-import AWS from 'aws-sdk'
+import AWS from "aws-sdk";
 
 AWS.config.update({
   accessKeyId: process.env.AWS_SES_KEY,
   secretAccessKey: process.env.AWS_SES_SECRET,
-  region: process.env.AWS_SES_REGION
-})
+  region: process.env.AWS_SES_REGION,
+});
 
 export default (req, res) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const emailHTML = `
       <h1>Solicitud de contacto</h1>
       <h2>Enviado por: ${req.body.name}</h2>
       <h2>Correo: ${req.body.email}</h2>
       <h3>Pregunta:</h3>
       <p>${req.body.question}</p>
-    `
+    `;
     const emailParams = {
       Destination: {
-        ToAddresses: ['banuelosmd@gmail.com'] 
+        ToAddresses: ["banuelosmd@gmail.com"],
       },
       Message: {
         Subject: {
-          Charset: 'UTF-8',
-          Data: 'Pregunta en el sitio web drjesusbanuelos.com'
+          Charset: "UTF-8",
+          Data: "Pregunta en el sitio web drjesusbanuelos.com",
         },
         Body: {
           Html: {
-            Charset: 'UTF-8',
-            Data: emailHTML
-          }
-        }
+            Charset: "UTF-8",
+            Data: emailHTML,
+          },
+        },
       },
-      Source: 'noreply@drjesusbanuelos.com'
-    }
+      Source: "noreply@drjesusbanuelos.com",
+    };
 
-    const sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(emailParams).promise();
+    const sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+      .sendEmail(emailParams)
+      .promise();
 
     return sendPromise
-            .then(data => res.status(200).json({ msg: 'email sent', data}))
-            .catch(err => res.status(500).json({ msg: 'something went wrong', err}))
-  } 
-  else {
-    res.json({ msg: 'holi'});
-  } 
-  
-}
+      .then((data) => res.status(200).json({ msg: "email sent", data }))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ msg: "something went wrong", err });
+      });
+  } else {
+    res.json({ msg: "holi" });
+  }
+};
--- src/components/contacto/ContactForm.js
+++ src/components/contacto/ContactForm.js
@@ -1,123 +1,136 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import axios from 'axios';
-import flasher from '../../utils/flasher';
-import useFormValidation from '../../utils/useFormValidation';
-import validateContactForm from '../../utils/validateContactForm';
+import { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import flasher from "../../utils/flasher";
+import useFormValidation from "../../utils/useFormValidation";
+import validateContactForm from "../../utils/validateContactForm";
 
-const ContactFormStyled = styled.form`    
-    margin-top: .5rem;
-    label {
-        display: block;
-        margin-top: 1rem;
-        margin-bottom: .2rem;
-    }
-    small {
-        position: absolute;
-        color: red;
-        font-weight: 300;
-    }
-    input {
-        display: block;
-        width: 99%;
-        border: 0;
-        border-bottom: 1px solid #666;
-        
-    }
-    textarea {
-        width: 99%;
-        height: 5rem;
-        display: block;
-        margin-top: .5rem;
-        resize: none;
-    }    
-    .error {
-        border-color: red;
-    }
-    input[type="submit"] {
-        margin-top: 1.5rem; 
-        height: 2rem;  
-        font-weight: 600; 
-        border-radius: 7px;    
-    }
-    input[type="submit"]:disabled {  
-        opacity: .5; 
-    }
-    input:focus, textarea:focus {
-        outline: none;
-    }
-`
+const ContactFormStyled = styled.form`
+  margin-top: 0.5rem;
+  label {
+    display: block;
+    margin-top: 1rem;
+    margin-bottom: 0.2rem;
+  }
+  small {
+    position: absolute;
+    color: red;
+    font-weight: 300;
+  }
+  input {
+    display: block;
+    width: 99%;
+    border: 0;
+    border-bottom: 1px solid #666;
+  }
+  textarea {
+    width: 99%;
+    height: 5rem;
+    display: block;
+    margin-top: 0.5rem;
+    resize: none;
+  }
+  .error {
+    border-color: red;
+  }
+  input[type="submit"] {
+    margin-top: 1.5rem;
+    height: 2rem;
+    font-weight: 600;
+    border-radius: 7px;
+  }
+  input[type="submit"]:disabled {
+    opacity: 0.5;
+  }
+  input:focus,
+  textarea:focus {
+    outline: none;
+  }
+`;
 const initialValues = {
-    name: '',
-    email: '',
-    question: ''
-}
+  name: "",
+  email: "",
+  question: "",
+};
 
 const ContactForm = () => {
-    const [sendingMail, setSendingMail] = useState(false)
+  const [sendingMail, setSendingMail] = useState(false);
 
-    const onValidValues = () => {        
-        // submit data
-        setSendingMail(true);
-        axios.post('/api/contact_form', values)
-        .then( res => {
-            if(res.status === 200) {
-                // success  
-                flasher('Gracias por tu contacto, reponderemos a la brevedad posible.', 'success');               
-                resetValues();
-                setSendingMail(false);
-            }
-            else {
-                // email sent error
-                flasher('Algo salió mal, intenta de nuevo más tarde.', 'error');                 
-            }
-        })
-        .catch(err => {
-            // axios error
-            console.error(err);
-            flasher('Algo salió mal, intenta de nuevo más tarde.', 'error');
-        });
-    }
-    
-    const { handleSubmit, handleBlur, handleChange, resetValues,
-            values, errors } = 
-            useFormValidation(initialValues, validateContactForm, onValidValues);
-    
-    return (        
-        <ContactFormStyled onSubmit={handleSubmit}>                      
-            <label htmlFor="name">nombre:</label>
-            <input 
-                onChange={handleChange}
-                onBlur={handleBlur}
-                type="text" 
-                id="name"
-                value={values.name}
-                className={ errors.name ? 'error' : ''}
-            />
-            { errors.name && <small>{errors.name}</small> }
-            <label htmlFor="email">email:</label>
-            <input 
-                onChange={handleChange}
-                onBlur={handleBlur}
-                type="email" 
-                id="email"
-                value={values.email}
-                className={ errors.email ? 'error' : ''}
-            />
-            { errors.email && <small>{errors.email}</small> }
-            <label htmlFor="question">pregunta:</label>
-            <textarea 
-                onChange={handleChange}
-                onBlur={handleBlur}                 
-                id="question"
-                value={values.question}
-                className={ errors.question ? 'error' : ''}
-            />
-            { errors.question && <small>{errors.question}</small> }
+  const onValidValues = () => {
+    // submit data
+    setSendingMail(true);
+    axios
+      .post("/api/contact_form", values)
+      .then((res) => {
+        if (res.status === 200) {
+          // success
+          flasher(
+            "Gracias por tu contacto, reponderemos a la brevedad posible.",
+            "success"
+          );
+          resetValues();
+        } else {
+          // email sent error
+          flasher("Algo salió mal, intenta de nuevo más tarde.", "error");
+          console.log(res);
+        }
+      })
+      .catch((err) => {
+        // axios error
+        console.error(err);
+        flasher("Algo salió mal, intenta de nuevo más tarde.", "error");
+      })
+      .finally(() => setSendingMail(false));
+  };
 
-            <input type="submit" className="link-btn primary-btn" disabled={sendingMail} />
-        </ContactFormStyled>
-    )
-}
+  const {
+    handleSubmit,
+    handleBlur,
+    handleChange,
+    resetValues,
+    values,
+    errors,
+  } = useFormValidation(initialValues, validateContactForm, onValidValues);
+
+  return (
+    <ContactFormStyled onSubmit={handleSubmit}>
+      <label htmlFor="name">nombre:</label>
+      <input
+        onChange={handleChange}
+        onBlur={handleBlur}
+        type="text"
+        id="name"
+        value={values.name}
+        className={errors.name ? "error" : ""}
+      />
+      {errors.name && <small>{errors.name}</small>}
+      <label htmlFor="email">email:</label>
+      <input
+        onChange={handleChange}
+        onBlur={handleBlur}
+        type="email"
+        id="email"
+        value={values.email}
+        className={errors.email ? "error" : ""}
+      />
+      {errors.email && <small>{errors.email}</small>}
+      <label htmlFor="question">pregunta:</label>
+      <textarea
+        onChange={handleChange}
+        onBlur={handleBlur}
+        id="question"
+        value={values.question}
+        className={errors.question ? "error" : ""}
+      />
+      {errors.question && <small>{errors.question}</small>}
+
+      <input
+        type="submit"
+        className="link-btn primary-btn"
+        disabled={sendingMail}
+      />
+    </ContactFormStyled>
+  );
+};
 
 export default ContactForm;
